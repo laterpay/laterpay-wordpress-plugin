9e29848782023779641799a5988b4416a15dbb20
drodler <drodler@laterpay.net>
2014-07-09 11:34:30 +0200

    #35 Clean up usermeta and postmeta table on uninstall

--- laterpay/application/core/LaterPay.php
+++ laterpay/application/core/LaterPay.php
@@ -105,9 +105,9 @@ class LaterPay {
 
         $this->setupPluginSettingsLink();
     }
-    
+
     /**
-     * 
+     *
      * @param string $settings
      */
     private function _generateUserSettings( $settings ) {
@@ -124,7 +124,7 @@ class LaterPay {
             ),
             $settings
         );
-        
+
         return $config;
     }
 
@@ -276,8 +276,8 @@ class LaterPay {
         );
 
         add_option('laterpay_plugin_is_activated',      '0');
-        add_option('laterpay_teaser_content_only',      '');
-        add_option('laterpay_plugin_is_in_live_mode',   '');
+        add_option('laterpay_teaser_content_only',      '1');
+        add_option('laterpay_plugin_is_in_live_mode',   '0');
         add_option('laterpay_sandbox_merchant_id',      '');
         add_option('laterpay_sandbox_api_key',          '');
         add_option('laterpay_live_merchant_id',         '');
@@ -309,12 +309,43 @@ class LaterPay {
         $table_terms_price  = $wpdb->prefix . 'laterpay_terms_price';
         $table_history      = $wpdb->prefix . 'laterpay_payment_history';
         $table_post_views   = $wpdb->prefix . 'laterpay_post_views';
+        $table_postmeta     = $wpdb->prefix . 'postmeta';
+        $table_usermeta     = $wpdb->prefix . 'usermeta';
 
+        // remove custom tables
         $sql = "DROP TABLE IF EXISTS
                     $table_currency,
                     $table_terms_price,
                     $table_history,
                     $table_post_views;
+                ";
+        $wpdb->query($sql);
+
+        // remove custom data from WP core tables
+        $sql = "DELETE FROM
+                    $table_postmeta
+                WHERE
+                    meta_key IN (
+                        'Teaser content',
+                        'Pricing Post',
+                        'Pricing Post Type',
+                        'laterpay_start_price',
+                        'laterpay_end_price',
+                        'laterpay_change_start_price_after_days',
+                        'laterpay_transitional_period_end_after_days',
+                        'laterpay_reach_end_price_after_days'
+                    )
+                ;
+                ";
+        $wpdb->query($sql);
+        $sql = "DELETE FROM
+                    $table_usermeta
+                WHERE
+                    meta_key IN (
+                        'laterpay_preview_post_as_visitor',
+                        'laterpay_hide_statistics_pane'
+                    )
+                ;
                 ";
         $wpdb->query($sql);
 
