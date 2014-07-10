baf0a182cd14523ea17321d148f4f40db125f0d7
Ralf Wiechers <drivingralle@me.com>
2014-07-10 23:53:49 +0200

    More nice plugin info comment

--- laterpay/laterpay.php
+++ laterpay/laterpay.php
@@ -1,11 +1,11 @@
 <?php
 /*
-  Plugin Name: LaterPay
-  Plugin URI: https://laterpay.net/developers/plugins-and-libraries
-  Description: This plugin integrates LaterPay into your blog. LaterPay is a new payment method for selling digital content with ease. Your users simply agree to pay later, get instant access to your content and pay once their invoice reaches 5 Euro. The set-up is fast and painless. You can set global, category, or individual prices and use extended pricing to change prices automatically over time to boost your sales. The plugin protects the files in your paid posts, provides sales statistics for your content, and is designed to work smoothly with social media plugins and crawlers in order to not reduce your blog's reach.
-  Author: LaterPay GmbH and Mihail Turalenka
-  Version: 0.9.5.1
-  Author URI: https://laterpay.net/
+ * Plugin Name: LaterPay
+ * Plugin URI: https://laterpay.net/developers/plugins-and-libraries
+ * Description: This plugin integrates LaterPay into your blog. LaterPay is a new payment method for selling digital content with ease. Your users simply agree to pay later, get instant access to your content and pay once their invoice reaches 5 Euro. The set-up is fast and painless. You can set global, category, or individual prices and use extended pricing to change prices automatically over time to boost your sales. The plugin protects the files in your paid posts, provides sales statistics for your content, and is designed to work smoothly with social media plugins and crawlers in order to not reduce your blog's reach.
+ * Author: LaterPay GmbH and Mihail Turalenka
+ * Version: 0.9.5
+ * Author URI: https://laterpay.net/
  */
 
 $laterpay_version = '0.9.5.1';
