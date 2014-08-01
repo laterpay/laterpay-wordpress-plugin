fc9b6b52b6844a046bd03306f3aa78ec6430ea39
drodler <drodler@laterpay.net>
2014-08-01 16:27:13 +0200

    #187 Removed empty index.php files

--- laterpay/assets/css/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-// This index.php protects the files in this folder against direct access from the internet
-// This index.php protects the files in its folder against direct access from the internet
--- laterpay/library/laterpay/LaterPay/Core/Logger/index.php
+++
@@ -1,2 +0,0 @@
-<?php
-// This index.php protects the files in its folder against direct access from the internet
