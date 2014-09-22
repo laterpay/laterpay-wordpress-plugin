4c3a25350cac242c1ab39bfbaac736715e9f830e
Tsumarov, Aleksandr <atsumarov@scnsoft.com>
2014-09-22 16:09:24 +0300

    #308 Fix scenario. Used: firefox v31, Plugin v0.9.7.1-128-gd6b69b8, Wordpress v.4.1-alpha-src, OS v.Ubuntu

--- laterpay/selenium-tests/tests/backend/BasicCest.php
+++ laterpay/selenium-tests/tests/backend/BasicCest.php
@@ -808,7 +808,7 @@ class SetupPluginCest {
 
         PostModule::of($I)->checkTestPostForLaterPayElements($_testPost2, 'individual price', $_priceTwo, $_currency, BaseModule::$T3);
 
-        PostModule::of($I)->checkIfWrongShortcodeIsDisplayedCorrectly($_testPost1, $_priceOne);
+        PostModule::of($I)->checkIfWrongShortcodeIsDisplayedCorrectly($_testPost2, $_priceTwo);
     }
 
     /**
