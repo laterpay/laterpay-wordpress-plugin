1d740e034b3bab84c999eb149654e1dfcbf1091c
Christian Brückner <family.brueckner@googlemail.com>
2014-10-06 15:13:56 +0200

    added toggle visibility for tabs #266

--- laterpay/assets/js/laterpay-debugger.js
+++ laterpay/assets/js/laterpay-debugger.js
@@ -4,16 +4,22 @@
 			var $o = {
 					menuItem: $( '#wp-admin-bar-lp_js_debugger-admin-bar-menu' ),
 					debugger: $( '.lp_debugger' ),
+					tabItem: $( '.lp_debugger-tab a' ),
 
-					hiddenClass: 'lp_debugger_is_hidden',
-					visibleClass: 'lp_debugger_is_visible',
+					tabContentSelector: '.lp_debugger-content',
+
+					hiddenTabClass: 'lp_debugger_tab_is_hidden',
+					visibleTabClass: 'lp_debugger_tab_is_visible',
+
+					hiddenDebuggerClass: 'lp_debugger_is_hidden',
+					visibleDebuggerClass: 'lp_debugger_is_visible',
 
 					hiddenAttr: { 'aria-hidden' : true, 'aria-visible' : false },
 					visibleAttr: { 'aria-hidden' : false, 'aria-visible' : true }
 				},
 
 
-				bindEvents = function () {
+				bindDebugbarEvents = function () {
 
 					$o.menuItem
 						.on( 'mousedown', function () {
@@ -25,13 +31,39 @@
 						.attr({'role' : 'button'});
 				},
 
-				handleMenuItemEvent = function () {
+				bindTabEvents = function () {
 
-					if( $o.debugger.hasClass( $o.hiddenClass ) ){
-						$o.debugger.removeClass( $o.hiddenClass ).addClass( $o.visibleClass ).attr( $o.visibleAttr );
+					$o.tabItem
+						.on( 'mousedown', function () {
+							handleTabEvent( this );
+						} )
+						.on( 'click', function ( e ) {
+							e.preventDefault();
+						} )
+
+				},
+
+				handleTabEvent = function ( that ) {
+					var target = $( that ).attr( 'href' ),
+						$tabContainer = $( target )
+					;
+
+					if( $tabContainer.hasClass( $o.hiddenTabClass ) ){
+						$tabContainer.removeClass( $o.hiddenTabClass ).addClass( $o.visibleTabClass ).attr( $o.visibleAttr );
 					}
 					else {
-						$o.debugger.removeClass( $o.visibleClass ).addClass( $o.hiddenClass ).attr( $o.hiddenAttr );
+						$tabContainer.removeClass( $o.visibleTabClass ).addClass( $o.hiddenTabClass ).attr( $o.visibleAttr );
+					}
+
+				},
+
+				handleMenuItemEvent = function () {
+
+					if( $o.debugger.hasClass( $o.hiddenDebuggerClass ) ){
+						$o.debugger.removeClass( $o.hiddenDebuggerClass ).addClass( $o.visibleDebuggerClass ).attr( $o.visibleAttr );
+					}
+					else {
+						$o.debugger.removeClass( $o.visibleDebuggerClass ).addClass( $o.hiddenDebuggerClass );
 					}
 
 
@@ -39,9 +71,14 @@
 
 				initialize = function(){
 
-					$o.debugger.addClass( $o.hiddenClass ).attr( $o.hiddenAttr );
+					// hide the debugger
+					$o.debugger.addClass( $o.hiddenDebuggerClass ).attr( $o.hiddenAttr );
 
-					bindEvents();
+					// hide all tabs
+					$( $o.tabItem ).parent().addClass( $o.hiddenTabClass ).attr( $o.hiddenAttr );
+
+					bindDebugbarEvents();
+					bindTabEvents();
 				}
 
 
