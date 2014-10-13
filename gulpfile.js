c3337316f028ce5ce9860926677f1dee94fd6082
drodler <drodler@laterpay.net>
2014-10-13 23:31:29 +0200

    #392 Added svgmin

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,7 @@
 var autoprefixer    = require('gulp-autoprefixer'),
-    // browserSync     = require('browser-sync'),
     cached          = require('gulp-cached'),
     // changed         = require('gulp-changed'),
-    concat          = require('gulp-concat'),
+    closure         = require('gulp-closure-compiler-service'),
     csso            = require('gulp-csso'),
     csslint         = require('gulp-csslint'),
     del             = require('del'),
@@ -14,7 +13,6 @@ var autoprefixer    = require('gulp-autoprefixer'),
     lintspaces      = require('gulp-lintspaces'),
     notify          = require('gulp-notify'),
     phpcs           = require('gulp-phpcs'),
-    // reload          = browserSync.reload,
     rename          = require('gulp-rename'),
     size            = require('gulp-size'),
     // sourcemaps      = require('gulp-sourcemaps'),
@@ -22,15 +20,17 @@ var autoprefixer    = require('gulp-autoprefixer'),
     stripDebug      = require('gulp-strip-debug'),
     stylish         = require('jshint-stylish'),
     stylus          = require('gulp-stylus'),
-    // svgmin          = requre('gulp-svgmin'),
+    svgmin          = require('gulp-svgmin'),
     uglify          = require('gulp-uglify'),
     p               = {
                         allfiles    : ['./laterpay/**/*.php', './laterpay/assets/stylus/**/*.styl', './laterpay/assets/js/*.js'],
                         phpfiles    : ['./laterpay/**/*.php', '!./laterpay/library/**/*.php'],
                         srcStylus   : './laterpay/assets/stylus/*.styl',
                         srcJS       : './laterpay/assets/js_src/**/*.js',
+                        srcSVG      : './laterpay/assets/img/**/*.svg',
                         distJs      : './laterpay/assets/js/',
                         distCss     : './laterpay/assets/css/',
+                        distSVG     : './laterpay/assets/img/',
                     };
 
 
@@ -44,54 +44,57 @@ gulp.task('clean', function(cb) {
 gulp.task('css-watch', function() {
     gulp.src(p.srcStylus)
         .pipe(soften(4))
-        .pipe(stylus({                                              // process Stylus sources to CSS
-            linenos: true,                                          // make line numbers available in browser dev tools
-            // urlFunc: 'inline-image',                                // inline images where defined by background-image inline-image([url])
+        .pipe(stylus({                                                          // process Stylus sources to CSS
+            linenos: true,                                                      // make line numbers available in browser dev tools
+            // urlFunc: 'inline-image',                                         // inline images where defined by background-image inline-image([url])
             // TODO: generate sourcemap
         }))
         .on('error', notify.onError())
-        .pipe(gulp.dest(p.distCss));                                 // move to target folder
-        // .pipe(reload({stream: true}));
+        .pipe(gulp.dest(p.distCss));                                            // move to target folder
 });
 
 gulp.task('css-build', function() {
     gulp.src(p.srcStylus)
         .pipe(soften(4))
-        .pipe(stylus({                                              // process Stylus sources to CSS
-            linenos: false,                                         // make line numbers available in browser dev tools
+        .pipe(stylus({                                                          // process Stylus sources to CSS
+            linenos: false,                                                     // make line numbers available in browser dev tools
             compress: true,
-            // urlFunc: 'inline-image',                                // inline images where defined by background-image inline-image([url])
+            // urlFunc: 'inline-image',                                         // inline images where defined by background-image inline-image([url])
         }))
         .on('error', notify.onError())
-        // .pipe(csslint())                                            // lint with CSSLint
-        // .pipe(csslint.reporter())
-        // .pipe(autoprefixer('last 3 versions', '> 2%', 'ff > 23', 'ie > 7')) // vendorize properties for supported browsers
-        // .pipe(csso())                                               // compress with csso
-        .pipe(gulp.dest(p.distCss));                                 // move to target folder
+        // .pipe(autoprefixer('last 3 versions', '> 2%', 'ff > 23', 'ie > 7'))  // vendorize properties for supported browsers
+        // .pipe(csso())                                                        // compress with csso
+        .pipe(gulp.dest(p.distCss));                                            // move to target folder
 });
 
 // Javascript related tasks
 gulp.task('js-watch', function() {
     gulp.src(p.srcJS)
-        .pipe(cached('hinting'))                                // only process modified files
+        .pipe(cached('hinting'))                                                // only process modified files
             .pipe(soften(4))
-            .pipe(jshint('.jshintrc'))                          // lint with JSHint
-            .pipe(jshint.reporter(stylish))                     // output JSHint results
-            // .pipe(fixmyjs())                                    // fix JSHint errors if possible
-            .pipe(gulp.dest(p.distJs))                          // move to target folder
+            .pipe(jshint('.jshintrc'))                                          // lint with JSHint
+            .pipe(jshint.reporter(stylish))                                     // output JSHint results
+            // .pipe(fixmyjs())                                                 // fix JSHint errors, if possible
+            .pipe(gulp.dest(p.distJs))                                          // move to target folder
             .pipe(notify({message: 'JS task complete :-)'}));
 });
 
 gulp.task('js-build', function() {
     gulp.src(p.srcJS)
         .pipe(soften(4))
-        .pipe(stripDebug())                                 // remove console, alert, and debugger statements
-        .pipe(jshint('.jshintrc'))                          // lint with JSHint
-        .pipe(jshint.reporter(stylish))                     // output JSHint results
-        // .pipe(fixmyjs())                                    // fix JSHint errors if possible
-        // .pipe(concat('main.js'))                         // concatenate files
-        .pipe(uglify())                                     // compress with uglify
-        .pipe(gulp.dest(p.distJs));                          // move to target folder;
+        .pipe(stripDebug())                                                     // remove console, alert, and debugger statements
+        // .pipe(jshint('.jshintrc'))                                              // lint with JSHint
+        // .pipe(jshint.reporter(stylish))                                         // output JSHint results
+        // .pipe(fixmyjs())                                                     // fix JSHint errors if possible
+        .pipe(uglify())                                                         // compress with uglify
+        .pipe(gulp.dest(p.distJs));                                             // move to target folder
+});
+
+// Image related tasks
+gulp.task('svg-build', function() {
+    gulp.src(p.srcSVG)
+        .pipe(svgmin())                                                         // compress with svgmin
+        .pipe(gulp.dest(p.distSVG));                                            // move to target folder
 });
 
 // ensure consistent whitespace etc. in files
@@ -110,13 +113,12 @@ gulp.task('fileformat', function() {
 // check PHP coding standards
 gulp.task('sniffphp', function() {
     return gulp.src(p.phpfiles)
-            .soften(4)
             .pipe(phpcs({
                 bin             : '/usr/local/bin/phpcs',
                 standard        : 'WordPress',
                 warningSeverity : 0
             }))
-            .pipe(phpcs.reporter());
+            .pipe(phpcs.reporter('log'));
 });
 
 // update git submodules
@@ -141,10 +143,23 @@ gulp.task('default', ['clean', 'css-watch', 'js-watch'], function() {
     gulp.watch(p.srcJS,     ['js-watch']);
 });
 
+// check code quality before git commit
+gulp.task('precommit', ['sniffphp'], function() {
+    gulp.src(p.srcJS)
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter(stylish));
+        // .pipe(fixmyjs())
+
+    gulp.src(p.srcStylus)
+        .pipe(csslint())
+        .pipe(csslint.reporter());
+});
+
 // build project for release
 // gulp.task('build', ['clean', 'updateSubmodules', 'css', 'js'], function() {
-gulp.task('build', ['clean', 'updateSubmodules', 'fileformat', 'sniffphp'], function() {
-    // git archive is the right option to export the entire repo
+gulp.task('build', ['clean', 'updateSubmodules'], function() {
+    // TODO: git archive is the right option to export the entire repo
     gulp.start('css-build');
     gulp.start('js-build');
+    gulp.start('svg-build');
 });
