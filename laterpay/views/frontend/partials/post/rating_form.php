0bc4f9103a8b52119f8cb5cc8933a8717c66a838
drodler <drodler@laterpay.net>
2014-11-18 09:42:10 +0100

    #163 Display half stars for .5 ratings

--- laterpay/views/frontend/partials/post/rating_form.php
+++ laterpay/views/frontend/partials/post/rating_form.php
@@ -9,23 +9,28 @@
             <?php echo __( 'Please rate this post:', 'laterpay' ); ?>
             <input type="radio" name="rating_value" id="lp_rating__input-5" class="lp_rating__input" value="5">
             <label for="lp_rating__input-5" class="lp_rating__star" title="5 <?php echo __( 'stars', 'laterpay' ); ?>">
-                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 14 14" enable-background="new 0 0 14 14" xml:space="preserve"><polygon points="7,0.4 9.2,4.8 14,5.5 10.5,8.9 11.3,13.7 7,11.4 2.7,13.7 3.5,8.9 0,5.5 4.8,4.8"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 14 14" enable-background="new 0 0 14 14" xml:space="preserve"><polygon class="lp_star__full" points="7,0.4 9.2,4.8 14,5.5 10.5,8.9 11.3,13.7 7,11.4 2.7,13.7 3.5,8.9 0,5.5 4.8,4.8"/><polygon class="lp_star__half" points="7,11.4 7,0.4 4.8,4.8 0,5.4 3.5,8.9 2.7,13.6"/></svg>
+
             </label>
             <input type="radio" name="rating_value" id="lp_rating__input-4" class="lp_rating__input" value="4">
             <label for="lp_rating__input-4" class="lp_rating__star" title="4 <?php echo __( 'stars', 'laterpay' ); ?>">
-                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 14 14" enable-background="new 0 0 14 14" xml:space="preserve"><polygon points="7,0.4 9.2,4.8 14,5.5 10.5,8.9 11.3,13.7 7,11.4 2.7,13.7 3.5,8.9 0,5.5 4.8,4.8"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 14 14" enable-background="new 0 0 14 14" xml:space="preserve"><polygon class="lp_star__full" points="7,0.4 9.2,4.8 14,5.5 10.5,8.9 11.3,13.7 7,11.4 2.7,13.7 3.5,8.9 0,5.5 4.8,4.8"/><polygon class="lp_star__half" points="7,11.4 7,0.4 4.8,4.8 0,5.4 3.5,8.9 2.7,13.6"/></svg>
+
             </label>
             <input type="radio" name="rating_value" id="lp_rating__input-3" class="lp_rating__input" value="3">
             <label for="lp_rating__input-3" class="lp_rating__star" title="3 <?php echo __( 'stars', 'laterpay' ); ?>">
-                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 14 14" enable-background="new 0 0 14 14" xml:space="preserve"><polygon points="7,0.4 9.2,4.8 14,5.5 10.5,8.9 11.3,13.7 7,11.4 2.7,13.7 3.5,8.9 0,5.5 4.8,4.8"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 14 14" enable-background="new 0 0 14 14" xml:space="preserve"><polygon class="lp_star__full" points="7,0.4 9.2,4.8 14,5.5 10.5,8.9 11.3,13.7 7,11.4 2.7,13.7 3.5,8.9 0,5.5 4.8,4.8"/><polygon class="lp_star__half" points="7,11.4 7,0.4 4.8,4.8 0,5.4 3.5,8.9 2.7,13.6"/></svg>
+
             </label>
             <input type="radio" name="rating_value" id="lp_rating__input-2" class="lp_rating__input" value="2">
             <label for="lp_rating__input-2" class="lp_rating__star" title="2 <?php echo __( 'stars', 'laterpay' ); ?>">
-                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 14 14" enable-background="new 0 0 14 14" xml:space="preserve"><polygon points="7,0.4 9.2,4.8 14,5.5 10.5,8.9 11.3,13.7 7,11.4 2.7,13.7 3.5,8.9 0,5.5 4.8,4.8"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 14 14" enable-background="new 0 0 14 14" xml:space="preserve"><polygon class="lp_star__full" points="7,0.4 9.2,4.8 14,5.5 10.5,8.9 11.3,13.7 7,11.4 2.7,13.7 3.5,8.9 0,5.5 4.8,4.8"/><polygon class="lp_star__half" points="7,11.4 7,0.4 4.8,4.8 0,5.4 3.5,8.9 2.7,13.6"/></svg>
+
             </label>
             <input type="radio" name="rating_value" id="lp_rating__input-1" class="lp_rating__input" value="1">
             <label for="lp_rating__input-1" class="lp_rating__star" title="1 <?php echo __( 'star', 'laterpay' ); ?>">
-                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 14 14" enable-background="new 0 0 14 14" xml:space="preserve"><polygon points="7,0.4 9.2,4.8 14,5.5 10.5,8.9 11.3,13.7 7,11.4 2.7,13.7 3.5,8.9 0,5.5 4.8,4.8"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 14 14" enable-background="new 0 0 14 14" xml:space="preserve"><polygon class="lp_star__full" points="7,0.4 9.2,4.8 14,5.5 10.5,8.9 11.3,13.7 7,11.4 2.7,13.7 3.5,8.9 0,5.5 4.8,4.8"/><polygon class="lp_star__half" points="7,11.4 7,0.4 4.8,4.8 0,5.4 3.5,8.9 2.7,13.6"/></svg>
+
             </label>
         </div>
     </form>
