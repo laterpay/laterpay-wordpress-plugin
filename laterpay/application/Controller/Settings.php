c1c75ea33e155355d9b605c646de08429e8b4b72
Aliaksandr Vahura <avahura@scnsoft.com>
2014-12-05 13:24:45 +0300

    #408. js and ajax removed.

--- laterpay/application/Controller/Settings.php
+++ laterpay/application/Controller/Settings.php
@@ -3,74 +3,6 @@
 class LaterPay_Controller_Settings extends LaterPay_Controller_Abstract
 {
     /**
-     * @see LaterPay_Controller_Abstract::load_assets()
-     */
-    public function load_assets() {
-        parent::load_assets();
-
-        // load page-specific CSS
-        // TODO: add styles if needed
-
-        // load page-specific JS
-        wp_register_script(
-            'laterpay-backend-settings',
-            $this->config->get( 'js_url' ) . 'laterpay-backend-settings.js',
-            array( 'jquery'),
-            $this->config->get( 'version' ),
-            true
-        );
-
-        wp_enqueue_script( 'laterpay-backend-settings' );
-
-        // add lpVars if needed
-        wp_localize_script(
-            'laterpay-backend-settings',
-            'lpVars',
-            array(
-            )
-        );
-    }
-
-    /**
-     * Ajax method to save laterpay advanced settings
-     *
-     * @wp-hook wp_ajax_laterpay_advanced_settings
-     */
-    public function save_laterpay_advanced_settings() {
-        $advanced_settings_form = new LaterPay_Form_Settings( $_POST );
-
-        if ( $advanced_settings_form->is_valid() ) {
-            global $wp_roles;
-            $unlimited_post_access = $advanced_settings_form->get_field_value( 'unlimited_post_access' );
-
-            foreach( $wp_roles->roles as $role => $role_data ) {
-                $role_obj = get_role( $role );
-                if ( ! $unlimited_post_access ) {
-                    $role_obj->remove_cap( 'laterpay_has_full_access_to_content' );
-                } else {
-                    if ( in_array( $role, $unlimited_post_access ) ) {
-                        $role_obj->add_cap( 'laterpay_has_full_access_to_content' );
-                    } else {
-                        $role_obj->remove_cap( 'laterpay_has_full_access_to_content' );
-                    }
-                }
-            }
-
-            wp_send_json(
-                array(
-                    'success' => true,
-                )
-            );
-        }
-
-        wp_send_json(
-            array(
-                'success' => false,
-            )
-        );
-    }
-
-    /**
      * Add LaterPay advanced settings to the settings menu.
      *
      * @return void
@@ -91,8 +23,6 @@ class LaterPay_Controller_Settings extends LaterPay_Controller_Abstract
      * @return string
      */
     public function render_advanced_settings_page() {
-        $this->load_assets();
-
         $view_args = array(
             'settings_title' => __( 'LaterPay Advanced Settings', 'laterpay'),
         );
