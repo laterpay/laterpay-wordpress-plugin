415841917e5514910b37b5d23fe262a51d21f10d
drodler <drodler@laterpay.net>
2014-12-05 01:35:42 +0100

    #408 Added standard wrapper to options page

--- laterpay/views/backend/options.php
+++ laterpay/views/backend/options.php
@@ -1,14 +1,16 @@
 <?php if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
 <?php
     if ( ! current_user_can( 'manage_options' ) ) {
-        wp_die( __( 'You do not have sufficient permissions to manage options for this site.' ) );
+        wp_die( __( 'You do not have sufficient permissions to manage options for this site.', 'laterpay' ) );
     }
 ?>
 
-<h2><?php echo $laterpay['settings_title']; ?></h2>
+<div class="wrap">
+    <h2><?php echo $laterpay['settings_title']; ?></h2>
 
-<form method="POST" action="options.php">
-    <?php settings_fields( 'laterpay' ); ?>
-    <?php do_settings_sections( 'laterpay' ); ?>
-    <?php submit_button(); ?>
-</form>
+    <form method="POST" action="options.php">
+        <?php settings_fields( 'laterpay' ); ?>
+        <?php do_settings_sections( 'laterpay' ); ?>
+        <?php submit_button(); ?>
+    </form>
+</div>
