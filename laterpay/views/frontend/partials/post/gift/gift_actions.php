24ebaf1293dcee902316576c81601b4c3f036a3e
drodler <drodler@laterpay.net>
2014-12-19 18:08:49 +0100

    #534 Layout fix for Substanz
    Revert for actual release??

--- laterpay/views/frontend/partials/post/gift/gift_actions.php
+++ laterpay/views/frontend/partials/post/gift/gift_actions.php
@@ -12,8 +12,10 @@
     <?php if ( $laterpay['has_access'] ): ?>
         <?php _e( 'Gift Code', 'laterpay' ); ?>
         <span class="lp_voucherCodeLabel"><?php echo $laterpay['gift_code']; ?></span><br>
+        <!--
         <?php _e( 'Redeem at', 'laterpay' ); ?>
         <a href="<?php echo $laterpay['landing_page']; ?>"><?php echo $laterpay['landing_page']; ?></a>
+        -->
     <?php else: ?>
         <a href="#" class="lp_js_doPurchase lp_purchaseLink lp_button" title="<?php echo __( 'Buy now with LaterPay', 'laterpay' ); ?>" data-icon="b" data-laterpay="<?php echo $pass['url']; ?>" data-preview-as-visitor="<?php echo $laterpay['preview_post_as_visitor']?>"><?php echo $title; ?></a>
     <?php endif; ?>
