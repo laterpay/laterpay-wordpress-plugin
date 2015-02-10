1bd574f0326f83f09972ef81e4478e58cc393737
Aliaksandr Vahura <avahura@scnsoft.com>
2015-02-10 14:41:42 +0300

    #573. Fixed get category prices via ajax, included subcategories, added duplicate rows prevention.

--- laterpay/application/Helper/Pricing.php
+++ laterpay/application/Helper/Pricing.php
@@ -887,21 +887,33 @@ class LaterPay_Helper_Pricing
         }
     }
 
+    /**
+     * Get category price data by category ids
+     *
+     * @param $category_ids
+     *
+     * @return array
+     */
     public static function get_category_price_data_by_category_ids( $category_ids ) {
         $result = array();
 
-        if ( is_array( $category_ids ) && $category_ids ) {
+        if ( is_array( $category_ids ) && count( $category_ids ) > 0 ) {
+            // this array will prevent category prices from duplication
+            $ids_used = array();
             $laterpay_category_model = new LaterPay_Model_CategoryPrice();
-            $category_price_data     = (array) $laterpay_category_model->get_category_price_data_by_category_ids( $category_ids );
-            // add data to result array
+            $category_price_data     = $laterpay_category_model->get_category_price_data_by_category_ids( $category_ids );
+            // add prices data to result array
             foreach ( $category_price_data as $category ) {
-                $result[] = (array) $category;
+                $ids_used[] = $category->category_id;
+                $result[]   = (array) $category;
             }
 
+            // loop through passed categoryies and check if they have category price
+            // if not then try to get parent category category price
             foreach ( $category_ids as $category_id ) {
                 $has_price = false;
                 foreach ( $category_price_data as $category ) {
-                    if ( $category['category_id'] == $category_id ) {
+                    if ( $category->category_id == $category_id ) {
                         $has_price = true;
                         break;
                     }
@@ -914,7 +926,11 @@ class LaterPay_Helper_Pricing
                         if ( ! $parent_data ) {
                             $parent_id = get_category( $parent_id )->parent;
                         } else {
-                            $result[] = (array) $parent_data[0];
+                            $parent_data = (array) $parent_data[0];
+                            if ( ! in_array( $parent_data['category_id'], $ids_used ) ) {
+                                $ids_used[] = $parent_data['category_id'];
+                                $result[]   = $parent_data;
+                            }
                             break;
                         }
                     }
