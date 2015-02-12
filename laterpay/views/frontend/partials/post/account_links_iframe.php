f34c13cddb8d2b4d0a91370100374ecb2f82e852
Aliaksandr Vahura <avahura@scnsoft.com>
2015-02-12 14:27:20 +0300

    #572. Fixed issues with do_action on purchased content.

--- laterpay/views/frontend/partials/post/account_links_iframe.php
+++ laterpay/views/frontend/partials/post/account_links_iframe.php
@@ -3,11 +3,11 @@
 <div>
     <?php if ( defined( 'DOING_AJAX' ) && DOING_AJAX ): ?>
         <?php
-        ob_start();
-        do_action( 'laterpay_account_links', $laterpay['css'], $laterpay['forcelang'], $laterpay['show'], $laterpay['next'] );
-        $html = ob_get_contents();
-        ob_clean();
-        echo $html;
+            ob_start();
+            do_action( 'laterpay_account_links', $laterpay['css'], $laterpay['forcelang'], $laterpay['show'], $laterpay['next'] );
+            $html = ob_get_contents();
+            ob_clean();
+            echo $html;
         ?>
     <?php else: ?>
         <?php do_action( 'laterpay_account_links', $laterpay['css'], $laterpay['forcelang'], $laterpay['show'], $laterpay['next'] ); ?>
