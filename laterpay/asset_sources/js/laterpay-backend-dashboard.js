d454f0e707493b52c05fe17b5dc223ef0c1b86bd
drodler <drodler@laterpay.net>
2015-02-25 11:36:18 +0100

    Alignment fixes for timepass lifecycle diagrams

--- laterpay/asset_sources/js/laterpay-backend-dashboard.js
+++ laterpay/asset_sources/js/laterpay-backend-dashboard.js
@@ -778,7 +778,7 @@
 
                     loadDashboardData('time_passes_expiry', refresh, timePassId)
                         .done(function(response) {
-                            var max             = parseInt(lpVars.maxYValue, 10) + 5, // add some air to y-axis scale
+                            var max             = parseInt(lpVars.maxYValue, 10) + 4, // add some air to y-axis scale
                                 $placeholder    = $(data[index]),
                                 markings        = [
                                     {
@@ -808,8 +808,8 @@
                                             to          : 3.25,
                                         },
                                         yaxis           : {
-                                            from        : max - 1,
-                                            to          : max - 1,
+                                            from        : max - 2,
+                                            to          : max - 2,
                                         },
                                     },
                                     {
@@ -821,8 +821,8 @@
                                             to          : 11.25,
                                         },
                                         yaxis           : {
-                                            from        : max - 1,
-                                            to          : max - 1,
+                                            from        : max - 2,
+                                            to          : max - 2,
                                         },
                                     },
                                 ],
@@ -912,7 +912,7 @@
                             $placeholder.append(xAxisLabel);
 
                             // add sum to 0-4 weeks interval
-                            var o4          = $graph.pointOffset({x: 1.5, y: max - 1}),
+                            var o4          = $graph.pointOffset({x: 1.5, y: max - 2}),
                                 sum04Label  = '<div class="lp_time-pass-diagram__sum" ' +
                                     'style="left:' + (o4.left - 30) + 'px; top:' + (o4.top - 14) + 'px;">' +
                                     sum04 +
@@ -920,7 +920,7 @@
                             $placeholder.append(sum04Label);
 
                             // add sum to 5-12 weeks interval
-                            var o5          = $graph.pointOffset({x: 7.5, y: max - 1}),
+                            var o5          = $graph.pointOffset({x: 7.5, y: max - 2}),
                                 sum512Label = '<div class="lp_time-pass-diagram__sum" ' +
                                     'style="left:' + (o5.left - 30) + 'px; top:' + (o5.top - 14) + 'px;">' +
                                     sum512 +
