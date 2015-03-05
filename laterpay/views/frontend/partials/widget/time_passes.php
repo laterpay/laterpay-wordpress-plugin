afbfcf51dec3a435aae6dceb77e28d719d628272
drodler <drodler@laterpay.net>
2015-03-05 14:39:18 +0100

    #661 Removed .lp_button from voucher code input button

--- laterpay/views/frontend/partials/widget/time_passes.php
+++ laterpay/views/frontend/partials/widget/time_passes.php
@@ -22,7 +22,7 @@
         <div id="lp_js_voucherCodeWrapper" class="lp_js_voucherCodeWrapper lp_timePassWidget_voucherCodeWrapper lp_u_clearfix">
             <input type="text" name="time_pass_voucher_code" class="lp_js_voucherCodeInput lp_timePassWidget_voucherCode" maxlength="6">
             <p class="lp_timePassWidget_voucherCodeInputHint"><?php _e( 'Code', 'laterpay' ); ?></p>
-            <div class="lp_js_voucherRedeemButton lp_timePassWidget_redeemVoucherCode lp_button"><?php _e( 'Redeem', 'laterpay' ); ?></div>
+            <div class="lp_js_voucherRedeemButton lp_timePassWidget_redeemVoucherCode"><?php _e( 'Redeem', 'laterpay' ); ?></div>
             <p class="lp_timePassWidget_voucherCodeHint"><?php _e( 'Redeem Voucher >', 'laterpay' ); ?></p>
         </div>
     <?php endif; ?>
