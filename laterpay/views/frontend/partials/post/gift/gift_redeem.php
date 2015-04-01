abc6c5dec6ff4e44532d7ca57815b6954b93394a
drodler <drodler@laterpay.net>
2015-04-01 10:16:23 +0200

    whitespace

--- laterpay/views/frontend/partials/post/gift/gift_redeem.php
+++ laterpay/views/frontend/partials/post/gift/gift_redeem.php
@@ -8,9 +8,9 @@
 <?php $gift_pass = $laterpay['pass_data']; ?>
 
 <div>
-    <?php if ( $gift_pass ) : ?>
+    <?php if ( $gift_pass ): ?>
         <?php echo $this->render_gift_pass( $gift_pass, true ); ?>
-    <?php else : ?>
+    <?php else: ?>
         <?php echo $this->render_redeem_form(); ?>
     <?php endif; ?>
 </div>
