c5d9f4488a49cae4398b72025fc2ee8c3fad7c45
drodler <drodler@laterpay.net>
2015-04-09 11:27:27 +0200

    Fixed conditions for automatic or manual rendering of purchase button

--- laterpay/laterpay.php
+++ laterpay/laterpay.php
@@ -189,7 +189,6 @@ function laterpay_get_plugin_config() {
         'content.preview_percentage_of_content'             => get_option( 'laterpay_preview_excerpt_percentage_of_content' ),
         'content.preview_word_count_min'                    => get_option( 'laterpay_preview_excerpt_word_count_min' ),
         'content.preview_word_count_max'                    => get_option( 'laterpay_preview_excerpt_word_count_max' ),
-        'content.show_purchase_button'                      => get_option( 'laterpay_show_purchase_button' ),
         'content.enabled_post_types'                        => $enabled_post_types ? $enabled_post_types : array(),
     );
     $config->import( $content_settings );
