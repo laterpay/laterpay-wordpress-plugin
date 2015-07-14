aceea82d4ae90c4ba8c17c8244f041f3fa94f505
Mihail Turalenka <mturalenka@scnsoft.com>
2015-07-14 11:40:07 +0300

    #612 fixed statistic tab appearance

--- laterpay/application/Hooks.php
+++ laterpay/application/Hooks.php
@@ -75,7 +75,7 @@ class LaterPay_Hooks {
             add_action( 'manage_' . $post_type . '_posts_custom_column',   array( $this, self::$wp_action_prefix . 'laterpay_post_custom_column_data' ), 10, 2 );
         }
 
-        add_action( 'wp_footer',                        array( $this, self::$wp_filter_prefix . 'laterpay_post_footer' ) );
+        add_action( 'wp_footer',                        array( $this, self::$wp_action_prefix . 'laterpay_post_footer' ) );
         add_action( 'wp_loaded',                        array( $this, self::$wp_action_prefix . 'laterpay_init_finished' ), 0 );
         add_action( 'template_redirect',                array( $this, self::$wp_action_prefix . 'laterpay_loaded' ) );
         add_action( 'wp_enqueue_scripts',               array( $this, self::$wp_action_prefix . 'laterpay_enqueue_scripts' ) );
