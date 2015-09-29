70e0717ac13a2f7b7c4f538a95728f3bfcf56b48
Vahura, Aliaksandr <avahura@scnsoft.com>
2015-09-29 14:15:10 +0300

    #842. Added spaces before/after brackets.

--- laterpay/views/frontend/partials/widget/purchase-button.php
+++ laterpay/views/frontend/partials/widget/purchase-button.php
@@ -39,4 +39,4 @@ if ( isset( $laterpay['link_text'] ) ) {
 ?>
 
 <div><a <?php echo laterpay_sanitized( $arg_str ); ?>><?php echo laterpay_sanitize_output( $link_text ); ?></a></div>
-<?php echo laterpay_sanitized($laterpay['sis_notification']); ?>
+<?php echo laterpay_sanitized( $laterpay['sis_notification'] ); ?>
