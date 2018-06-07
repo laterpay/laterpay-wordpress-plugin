cc3f7838ea036dfdc34b1d4b91d45a30cd67071c
Utkarsh Patel <itismeutkarsh@gmail.com>
2018-06-07 15:24:09 +0530

    Fix extra space escaping

--- laterpay/views/frontend/partials/post/teaser.php
+++ laterpay/views/frontend/partials/post/teaser.php
@@ -3,4 +3,4 @@ if ( ! defined( 'ABSPATH' ) ) {
     // prevent direct access to this file
     exit;
 }
-?><div class="lp_teaser-content"><?php echo LaterPay_Helper_View::remove_extra_spaces( wp_kses_post( $laterpay['teaser_content'] ) ); //phpcs:ignore ?></div>
+?><div class="lp_teaser-content"><?php echo wp_kses_post( LaterPay_Helper_View::remove_extra_spaces( $laterpay['teaser_content'] ) ); ?></div>
