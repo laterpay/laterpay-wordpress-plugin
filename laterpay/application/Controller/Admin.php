e1370bcae8bc121789fe9b4e2337f89f6e2ee2d6
Thrijith Thankachan <thrijith13@gmail.com>
2019-06-06 15:29:56 +0530

    Fix issue with update missing in Advanced tab and translation

--- laterpay/application/Controller/Admin.php
+++ laterpay/application/Controller/Admin.php
@@ -754,6 +754,7 @@ class LaterPay_Controller_Admin extends LaterPay_Controller_Base
         if ( ! empty( $ga_data['update_highlights']['version'] ) ) {
             $version_update_number                   = $ga_data['update_highlights']['version'];
             $ga_data['update_highlights']['version'] = sprintf( __( 'Version %s Highlights:', 'laterpay' ), $version_update_number );
+            $ga_data['update_highlights']['notice']  = sprintf( __( 'You can now fully customize the appearance of your payment overlay. Visit the Appearance tab, to check it out!', 'laterpay' ) );
             $ga_data['update_highlights_nonce']      = wp_create_nonce( 'update_highlights_nonce' );
             $ga_data['learn_more']                   = __( 'Learn More', 'laterpay' );
             $ga_data['ajaxUrl']                      = admin_url( 'admin-ajax.php' );
