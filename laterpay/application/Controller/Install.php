f8ee75565cc3615248797c7292271163c6be008c
Thrijith Thankachan <thrijith13@gmail.com>
2019-08-20 18:26:30 +0530

    Remove release notice for 2.6.3

--- laterpay/application/Controller/Install.php
+++ laterpay/application/Controller/Install.php
@@ -683,9 +683,7 @@ class LaterPay_Controller_Install extends LaterPay_Controller_Base
 
         if ( ! empty( $current_version ) ) {
 
-            $update_highlights = [
-                'version' => '2.6.2',
-            ];
+            $update_highlights = [];
 
             update_option( 'lp_update_highlights', $update_highlights );
             return;
